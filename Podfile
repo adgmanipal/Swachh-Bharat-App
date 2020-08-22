b6630f61ff94a6f57f55593714bf34b81c80b6e3
PragnyaDesh <60569711+PragnyaDesh@users.noreply.github.com>
2020-08-22 22:36:39 +0530

    register and login page

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwachhBharatApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod "TextFieldEffects" 
+
+  target 'SwachhBharatAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SwachhBharatAppUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ SwachhBharatApp/ViewControllers/RegisterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterViewController.swift
+//  SwachhBharatApp
+//
+//  Created by Pragnya Deshpande on 06/08/20.
+//  Copyright © 2020 PragnyaDesh. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     registerButton.layer.cornerRadius = 25.0
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
